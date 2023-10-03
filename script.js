b76fd855d3b9cf0c8ffeb23b80df40de2cabb10b
ProCoderAkhil <89849703+ProCoderAkhil@users.noreply.github.com>
2023-10-03 08:01:44 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function btClick(val)
+{
+    document.getElementById("display").value+=val;
+}
+function allClear()
+{
+    document.getElementById("display").value="";
+}
+function Clear()
+{
+   var current_ele= document.getElementById("display").value;
+   document.getElementById("display").value = current_ele.slice(0, -1);
+
+}
+function equal()
+{
+    var text=document.getElementById("display").value;
+    var result=eval(text);
+    document.getElementById("display").value=result;
+}
